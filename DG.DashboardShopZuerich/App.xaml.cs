ae8ea29ef7a5dfe4c14aa47756b7918c2b6e1ed1
samy <sam.vonbank@gmail.com>
2024-05-20 06:42:43 +0200

    Projektdateien hinzufügen.

---
+++ DG.DashboardShopZuerich/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DG.DashboardShopZuerich
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DG.DashboardShopZuerich/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DG.DashboardShopZuerich
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
