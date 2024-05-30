617010e0f2b5cca1dd2af5d820175313f692cdef
samy <sam.vonbank@gmail.com>
2024-05-30 18:27:46 +0200

    Update JasonService for lunchplan funktion

--- DG.DashboardShopZuerich/Models/LunchPlan.cs
+++ DG.DashboardShopZuerich/Models/LunchPlan.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DG.DashboardShopZuerich.Models
+﻿namespace DG.DashboardShopZuerich.Models
 {
     public class LunchPlan
     {
